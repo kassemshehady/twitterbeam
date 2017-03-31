7e7d72f64919824f062a9c5626e0e71ebe6b5f42
k.shehady <k.shehady@mentisnation.com>
2017-03-31 21:27:21 +0300

    first commit

---
+++ src/main/java/com/twitterbeam/app/BeamTwitterPipeline.java
@@ -0,0 +1,65 @@
+package com.twitterbeam.app;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.coders.BigEndianLongCoder;
+import org.apache.beam.sdk.coders.ByteArrayCoder;
+import org.apache.beam.sdk.coders.StringUtf8Coder;
+import org.apache.beam.sdk.io.PubsubIO;
+import org.apache.beam.sdk.io.kafka.KafkaIO;
+import org.apache.beam.sdk.io.kafka.KafkaRecord;
+import org.apache.beam.sdk.options.Default;
+import org.apache.beam.sdk.options.Description;
+import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.options.PipelineOptions.DirectRunner;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.transforms.DoFn.ProcessContext;
+import org.apache.beam.sdk.transforms.DoFn.ProcessElement;
+import org.apache.beam.sdk.values.PCollection;
+import org.apache.kafka.clients.consumer.OffsetResetStrategy;
+import org.apache.kafka.common.TopicPartition;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import avro.shaded.com.google.common.collect.ImmutableList;
+import avro.shaded.com.google.common.collect.ImmutableMap;
+
+/**
+ * Hello world!
+ *
+ */
+public class BeamTwitterPipeline {
+	private static final Logger LOG = LoggerFactory.getLogger(BeamTwitterPipeline.class);
+
+	public static void main(String[] args) {
+		TwitterBeamOptions options = PipelineOptionsFactory.fromArgs(args).withValidation()
+			      .as(TwitterBeamOptions.class);		
+		System.out.println("run");
+		Pipeline pipeline = Pipeline.create(options);
+		List<String> topics = ImmutableList.of("beamtwitter");
+		KafkaIO.Read<String, String> kafkareader = KafkaIO.<String, String>read()
+				.withBootstrapServers("localhost:9092")
+				.withTopics(topics)
+				 .withKeyCoder(StringUtf8Coder.of())
+			        .withValueCoder(StringUtf8Coder.of());
+
+		pipeline.apply(kafkareader).apply("ParseString", ParDo.of(new ParseString()));
+		
+		pipeline.run().waitUntilFinish();
+		
+	}
+	public static interface TwitterBeamOptions extends PipelineOptions {
+		  
+		}
+	static class ParseString extends DoFn<KafkaRecord<String, String>, String> {
+		@ProcessElement
+		public void processElement(ProcessContext c) {
+			System.out.println("Beam Twitter " + c.element().getKV().getValue());
+			c.output(new String(c.element().getKV().getValue()));
+		}
+	}
+}
---
+++ src/main/java/com/twitterbeam/app/KafkaTwitterProducer.java
@@ -0,0 +1,136 @@
+package com.twitterbeam.app;
+
+import java.util.Properties;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import twitter4j.*;
+import twitter4j.conf.*;
+
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+public class KafkaTwitterProducer {
+   public static void main(String[] args) throws Exception {
+      final LinkedBlockingQueue<Status> queue = new LinkedBlockingQueue<Status>(1000);
+      
+      
+    /*  if(args.length < 5){
+         System.out.println(
+            "Usage: KafkaTwitterProducer <twitter-consumer-key>
+            <twitter-consumer-secret> <twitter-access-token>
+            <twitter-access-token-secret>
+            <topic-name> <twitter-search-keywords>");
+         return;
+      }
+      
+      String consumerKey = args[0].toString();
+      String consumerSecret = args[1].toString();
+      String accessToken = args[2].toString();
+      String accessTokenSecret = args[3].toString();
+      String topicName = args[4].toString();
+      String[] arguments = args.clone();
+      String[] keyWords = Arrays.copyOfRange(arguments, 5, arguments.length);*/
+
+      String consumerKey = "DGzoa6HzYzZ8WIOqpyezCl9hW";
+      String consumerSecret = "B7ZFaFD2AsAa6ZLTXr5t6FVsdixoAp00dPiiZg8G9Pd9Ce1Sds";
+      String accessToken = "184713976-R8MrVgOo6TammQWhjF6V60HwsCMUG8bQFDexYk5F";
+      String accessTokenSecret = "0ri6LeL5kKpgfcWTXESHkDmelBiSQp1sIbf5az6EPtDmr";
+      String topicName = "beamtwitter";
+     /* String[] arguments = args.clone();*/
+      String[] keyWords = new String[]{"syria","isis"};
+      
+      ConfigurationBuilder cb = new ConfigurationBuilder();
+      cb.setDebugEnabled(true)
+         .setOAuthConsumerKey(consumerKey)
+         .setOAuthConsumerSecret(consumerSecret)
+         .setOAuthAccessToken(accessToken)
+         .setOAuthAccessTokenSecret(accessTokenSecret);
+
+      TwitterStream twitterStream = new TwitterStreamFactory(cb.build()).getInstance();
+      StatusListener listener = new StatusListener() {
+         //@Override
+         public void onStatus(Status status) {      
+            queue.offer(status);
+
+            // System.out.println("@" + status.getUser().getScreenName() 
+            //   + " - " + status.getText());
+            // System.out.println("@" + status.getUser().getScreen-Name());
+
+            /*for(URLEntity urle : status.getURLEntities()) {
+               System.out.println(urle.getDisplayURL());
+            }*/
+
+            /*for(HashtagEntity hashtage : status.getHashtagEntities()) {
+               System.out.println(hashtage.getText());
+            }*/
+         }       
+         public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
+            // System.out.println("Got a status deletion notice id:" 
+             //  + statusDeletionNotice.getStatusId());
+         }
+         
+         public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
+            // System.out.println("Got track limitation notice:" + 
+            //   numberOfLimitedStatuses);
+         }
+
+         public void onScrubGeo(long userId, long upToStatusId) {
+            // System.out.println("Got scrub_geo event userId:" + userId + 
+           // "upToStatusId:" + upToStatusId);
+         }      
+         
+         
+         public void onStallWarning(StallWarning warning) {
+            // System.out.println("Got stall warning:" + warning);
+         }
+         
+         
+         public void onException(Exception ex) {
+            ex.printStackTrace();
+         }
+      };
+      twitterStream.addListener(listener);
+      
+      FilterQuery query = new FilterQuery().track(keyWords);
+      twitterStream.filter(query);
+
+      Thread.sleep(5000);
+      
+      //Add Kafka producer config settings
+      Properties props = new Properties();
+      props.put("bootstrap.servers", "localhost:9092");
+      props.put("acks", "all");
+      props.put("retries", 0);
+      props.put("batch.size", 16384);
+      props.put("linger.ms", 1);
+      props.put("buffer.memory", 33554432);
+      
+      props.put("key.serializer", 
+         "org.apache.kafka.common.serialization.StringSerializer");
+      props.put("value.serializer", 
+         "org.apache.kafka.common.serialization.StringSerializer");
+      
+      Producer<String, String> producer = new KafkaProducer<String, String>(props);
+      int i = 0;
+      int j = 0;
+      
+      while(i < 1000) {
+         Status ret = queue.poll();
+         
+         if (ret == null) {
+            Thread.sleep(100);
+            i++;
+         }else {
+           // for(HashtagEntity hashtage : ret.getHashtagEntities()) {
+               System.out.println("Kafka Twitter: " + ret.getText());
+              producer.send(new ProducerRecord<String, String>(
+                  topicName, Integer.toString(j++), ret.getText()));
+            //}
+         }
+      }
+      producer.close();
+      Thread.sleep(5000);
+      twitterStream.shutdown();
+   }
+}
